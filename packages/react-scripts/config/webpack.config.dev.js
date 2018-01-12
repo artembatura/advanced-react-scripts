10a76bf62e8905ad01aa7684d27dc501fda72154
unknown <artemir.q@gmail.com>
2018-01-12 23:53:08 +0300

    Fixes

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -60,9 +60,6 @@ module.exports = {
     // require.resolve('webpack/hot/dev-server'),
     require.resolve('react-dev-utils/webpackHotDevClient'),
 
-    // #advanced-react-scripts
-    require.resolve('react-error-overlay'),
-
     // Finally, this is your app's code:
     paths.appIndexJs,
     // We include the app code last so that if there is a runtime error during
