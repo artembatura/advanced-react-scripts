50e8acd2c0e427acd61b6390bf9d5644f383a7e3
unknown <artemir.q@gmail.com>
2018-02-10 20:08:06 +0300

    Add babel-preset-stage-0. Updated dependencies in these packages: babel-plugin-named-asset-import-fresh, babel-preset-react-app-fresh, eslint-config-react-app-fresh, react-dev-utils-fresh, advanced-react-scripts

--- packages/react-scripts/config/advanced-react-scripts/config.js
+++ packages/react-scripts/config/advanced-react-scripts/config.js
@@ -1,19 +1,24 @@
 const mapObject = require('./utils/map-object');
 
 const customizers = {
-    webpackLoaders: require('./customizers/webpack-loaders')
+  babelPresets: {
+    BABEL_STAGE_0: {
+      get: () => require.resolve('babel-preset-stage-0'),
+    },
+  },
+  webpackLoaders: require('./customizers/webpack-loaders'),
 };
 
 module.exports = (env, isDev) => {
-    return mapObject(customizers, group => {
-        return mapObject(
-            group,
-            (customizer, key) => {
-                const envValue = process.env['REACT_APP_' + key];
-                const activeEnvValue = env && envValue && envValue !== 'false';
-                return (activeEnvValue || customizer.default) && customizer.get(isDev);
-            },
-            true
-        );
-    });
+  return mapObject(customizers, group => {
+    return mapObject(
+      group,
+      (customizer, key) => {
+        const envValue = process.env['REACT_APP_' + key];
+        const activeEnvValue = env && envValue && envValue !== 'false';
+        return (activeEnvValue || customizer.default) && customizer.get(isDev);
+      },
+      true
+    );
+  });
 };
